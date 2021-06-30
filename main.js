ca9f583509494479253c17221c62f37a5d983e3d
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-06-30 17:44:52 +0300

    finsh the project

--- main.js
+++ main.js
@@ -1,27 +1,114 @@
-// const mybtn = document.getElementsByClassName('buttonInTheFirstPage')
+// const myst = document.getElementsByClassName('buttonInTheFirstPage')
 
 const NextQuestion = document.querySelector(".button-in-the-theerd-page")
+const questionsCounter = document.querySelector(".Counter-of-questions")
+let mark = 0
+let questionList ={
+    1 :{
+        question : "How many students code academy ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 1,
+    },
 
-// console.log("Hello .....")
-let operationCount = 0 ;
+    2 :{
+        question : "How many students 2 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 2,
+    },
+
+    3 :{
+        question : "How many students 3 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 2,
+    },
+
+    4 :{
+        question : "How many students 4 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 4,
+    },
+
+    5 :{
+        question : "How many students 5 ?",
+        answers: [ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 1,
+    },
+
+    6 :{
+        question : "How many students 6 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 4,
+    },
+
+    7 :{
+        question : "How many students 7 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 2,
+    },
+
+    8 :{
+        question : "How many students 8 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 3,
+    },
+
+    9 :{
+        question : "How many students 9 ?",
+        answers: [ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 4,
+    },
+
+    10 :{
+        question : "How many students 10 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 2,
+    },
+
+    11 :{
+        question : "How many students 10 ?",
+        answers :[ '10' , '16' , '20' , '24'],
+        rightAnswerNo : 2,
+    },
+}
+
+let operationCount = 1 ;
 function moveOnTheNextQuestion (){
-    const removeTheDiv = document.querySelector(".questions").innerHTML = "Q.2 : How many asdasdasdasf ? "
     operationCount ++ ;
-    if (operationCount == 10){
+    const removeTheDiv = document.querySelector(".questions").innerHTML = questionList[operationCount ].question 
+    if (operationCount == 11){
         pageScore()
+    }else{
+        checkAnswer(operationCount)
     }
-    console.log(operationCount)
+    questionsCounter.innerHTML = operationCount +"/10"
 };
 
-function pageScore (){
+function pageScore (number){
     let NewPageHaveTheScore = document.querySelector(".mainDiv")
     NewPageHaveTheScore.remove();
+    creatResalt();
+};
+
+function creatResalt (){
+    let result = `<div class="heder-of-page"></div> 
+    <h2 class="headr1">your score : ${mark} </h2>`
+    let new_body = document.getElementById("body").innerHTML = result
 };
 
 let getSelectedValue = document.querySelector( 'input[name="answer"]:checked');
 
-// let numberOfQuestion = document.querySelector(".Counter-of-questions")
+function checkAnswer(number) {
+    var getSelectedValue = document.querySelector( 'input[name="answer"]:checked'); 
+    let index_of_answers = questionList[number]["answers"].indexOf(getSelectedValue.value)
 
+    if (index_of_answers+1 == questionList[number]["rightAnswerNo"]){
+        mark +=1 
+    }
+}
+
+// questionList.question
+
+// let numberOfQuestion = document.querySelector(".Counter-of-questions")
 
 // NextQuestion.onclick = function (){
 //     operationCount ++ ;
@@ -32,10 +119,6 @@ let getSelectedValue = document.querySelector( 'input[name="answer"]:checked');
 
 
 
-
-
-
-
 // const divImWantToRemove = document.children[0].children[1];
 
 // divImWantToRemove.removeChild( document.getElementsByClassName("center-qu"))
@@ -43,4 +126,3 @@ let getSelectedValue = document.querySelector( 'input[name="answer"]:checked');
 
 // let myobj = document.getElementById("all_content");
 //         myobj.remove();
-
